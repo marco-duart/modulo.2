dcd299968892c288dfac4e1c8b08ce9e5efb6c99
Marco Aurélio <marcoaurelio_db@hotmail.com>
2023-09-08 13:43:43 -0300

    restart

--- Typescript/sportBlog/src/pages/MainPage/styles.ts
+++
@@ -1,8 +0,0 @@
-/* import { styled } from 'styled-components'
-
-export const HeaderStyle = styled.header`
-    display: flex;
-    width: 100vw;
-    height: 92px;
-    background: rgba(105, 80, 161, 1);
-` */
